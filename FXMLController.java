0098ad9d21d75cdc0fa0156806e56626576f96b2
Martin Tastler <123328309+Martin2p@users.noreply.github.com>
2025-05-18 10:19:55 +0200

    Create FXMLController.java

---
+++ FXMLController.java
@@ -0,0 +1,18 @@
+package src;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class FXMLController {
+
+	/*
+	 * Deklarationen
+	 */
+		
+		@FXML private Stage meineBuehne;
+		@FXML private TextField eingabeAusgabe;
+	
+	
+}
